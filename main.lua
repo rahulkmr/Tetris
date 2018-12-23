e91da3713b9fca31152e2b2223ca106265d07d02
Rahul Kumar <rahul@thoughtnirvana.com>
2018-12-23 20:23:19 +0530

    Empty board

---
+++ main.lua
@@ -0,0 +1,28 @@
+function love.load()
+    love.graphics.setBackgroundColor(255, 255, 255)
+end
+
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.push('quit')
+    end
+end
+
+
+function love.draw()
+    for y = 1, 18 do
+        for x = 1, 10 do
+            love.graphics.setColor(.87, .87, .87)
+            local blockSize = 20
+            local blockDrawSize = blockSize - 1
+            love.graphics.rectangle(
+                'fill',
+                (x - 1) * blockSize,
+                (y - 1) * blockSize,
+                blockDrawSize,
+                blockDrawSize
+            )
+        end
+    end
+end
